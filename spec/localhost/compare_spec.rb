02bf40269cd24ce665eaab26ee3eb57eb0477cdd
Yuta Yasugahira <hourou_freak@yahoo.co.jp>
2017-02-08 12:50:19 +0000

    Implement loop prototype without jq

--- spec/localhost/compare_spec.rb
+++ spec/localhost/compare_spec.rb
@@ -9,10 +9,9 @@ after_fact = open("spec/localhost/after/centos7_ssh.json") do |io|
   JSON.load(io)
 end
 
-describe command("cat /Serverfact/spec/localhost/after/centos7_ssh.json | jq '.ansible_facts.ansible_local.service_enabled.\"httpd.service\"'") do
-  its(:stdout) { should match /#{before_fact['ansible_facts']['ansible_local']['service_enabled']['httpd.service']}/ }
-end
 
-describe command("cat /Serverfact/spec/localhost/after/centos7_ssh.json | jq '.ansible_facts.ansible_local.service_enabled.\"sshd.service\"'") do
-  its(:stdout) { should match /#{before_fact['ansible_facts']['ansible_local']['service_enabled']['sshd.service']}/ }
+after_fact['ansible_facts']['ansible_local']['service_enabled'].each do |service|
+  describe command("echo #{service}") do
+    its(:stdout) { should match /#{before_fact['ansible_facts']['ansible_local']['service_enabled']["#{service[0]}"]}/ }
+  end
 end
